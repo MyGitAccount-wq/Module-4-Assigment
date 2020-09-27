a697de5b78d66b0c1ea4a6202d96a3fa513c0173
MyGitAccount-wq <70694840+MyGitAccount-wq@users.noreply.github.com>
2020-09-27 20:49:43 +0630

    comit
    comit

---
+++ SpeakGoodBye.js
@@ -0,0 +1,9 @@
+(function(window) {
+	var word = "Good Bye";
+	var byeSpeaker = function (name) {
+  		console.log(word + " " + name);
+	}
+	
+	window.byeSpeaker = byeSpeaker;
+
+})(window);
---
+++ SpeakHello.js
@@ -0,0 +1,9 @@
+(function(window) {
+	var word = "Hello";
+	var helloSpeaker = function (name) {
+		console.log(word + " " + name);
+	}
+
+	window.helloSpeaker = helloSpeaker;
+
+})(window);
